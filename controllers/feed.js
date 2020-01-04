698ff6a1d76a8d6f3c58f6fa56db5b0d4ba92b36
JerryG09 <phemmy09@gmail.com>
2020-01-04 15:22:26 +0100

    implemented delete feed

--- controllers/feed.js
+++ controllers/feed.js
@@ -39,7 +39,7 @@ exports.createPost = (req, res, next) => {
     title: title,
     content: content,
     imageUrl: imageUrl,
-    creator: { name: 'Maximilian' }
+    creator: { name: 'Femi' }
   });
   post
     .save()
@@ -112,6 +112,31 @@ exports.updatePost = (req, res, next) => {
     })
     .then(result => {
       res.status(200).json({ message: 'Post updated!', post: result });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.deletePost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error('Could not find post.');
+        error.statusCode = 404;
+        throw error;
+      }
+      // Check logged in user
+      clearImage(post.imageUrl);
+      return Post.findByIdAndRemove(postId);
+    })
+    .then(result => {
+      console.log(result);
+      res.status(200).json({ message: 'Deleted post.' });
     })
     .catch(err => {
       if (!err.statusCode) {
